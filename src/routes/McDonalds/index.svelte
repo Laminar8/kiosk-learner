fdf2c228510d0a77a8500b77edc46601b8dde7f0
laminar8 <oliver_mique@aol.com>
2022-04-25 22:56:50 +0900

    McDonalds Update

---
+++ src/routes/McDonalds/index.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>McDonalds</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="McDonalds/ricottaCheese.png" alt="ricottaCheese" />
+				<img src="McDonalds/ricottaCheese.png" alt="ricottaCheese" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+</section>
